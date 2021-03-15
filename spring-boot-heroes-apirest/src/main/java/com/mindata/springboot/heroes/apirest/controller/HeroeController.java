7d0f0f345aebcbd8a02aa721031e4db9428ecf5a
Cristian <scristiansosa@gmail.com>
2021-03-15 01:33:59 -0300

    buscar Heroe por Id

--- spring-boot-heroes-apirest/src/main/java/com/mindata/springboot/heroes/apirest/controller/HeroeController.java
+++ spring-boot-heroes-apirest/src/main/java/com/mindata/springboot/heroes/apirest/controller/HeroeController.java
@@ -1,6 +1,7 @@
 package com.mindata.springboot.heroes.apirest.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,23 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mindata.springboot.heroes.apirest.entity.Heroe;
 import com.mindata.springboot.heroes.apirest.repository.HeroeRepository;
 
-
-
 @RestController
 public class HeroeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HeroeController.class);
 	@Autowired
 	private HeroeRepository heroeRepository;
-	
+
 	@PostMapping("crearHeroe")
 	public void save(Heroe heroe) {
 		heroeRepository.save(heroe);
 	}
-	
+
 	@GetMapping("/consultarHeroes")
 	public List<Heroe> findAll() {
 		return (List<Heroe>) heroeRepository.findAll();
 	}
-	
+
+	@GetMapping("/buscarHeroePorId")
+	public Optional<Heroe> findById(Long id) {
+		return heroeRepository.findById(id);
+	}
+
 }
