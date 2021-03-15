1f9d6173c514018a4e611657a39ee2ed5e3bf6b6
Cristian <scristiansosa@gmail.com>
2021-03-15 00:37:00 -0300

    git base

---
+++ spring-boot-heroes-apirest/src/test/java/com/mindata/springboot/heroes/apirest/SpringBootHeroesApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindata.springboot.heroes.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHeroesApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
